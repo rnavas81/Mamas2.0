27741783d4fd74425b86a88d0a8ab7a0dfb0965c
dario <darioleong1997@gmail.com>
2020-11-21 17:30:06 +0100

    Añadido controlador profesores

--- configuracion.php
+++ configuracion.php
@@ -15,6 +15,7 @@ const CTRL_BASICO = WEB_CTRL.DS.'basico.php';
 const CTRL_USUARIOS = WEB_CTRL.DS.'usuarios.php';
 const CTRL_ADMIN = WEB_CTRL.DS.'administrador.php';
 const CTRL_EXAMENES = WEB_CTRL.DS.'examenes.php';
+const CTRL_PROFESORES = WEB_CTRL.DS.'profesores.php';
 /*
 const CTRL_TAREAS = WEB_CTRL.DS.'tareas.php';
 */
