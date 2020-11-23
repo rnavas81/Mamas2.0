2b73c99b09f6c0498d00b43d5c7d35b5002dd60d
dario <darioleong1997@gmail.com>
2020-11-23 09:01:44 +0100

    Añadida tabla examenes

---
+++ docs/Examenes.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `Mamas`.`Examenes` ( 
+    `id` INT NOT NULL AUTO_INCREMENT , 
+    `idProfesor` INT NOT NULL ,
+    `habilitado` TINYINT NOT NULL DEFAULT '1' ,
+    `activo` TINYINT NOT NULL DEFAULT '0' ,
+    `nombre` VARCHAR(500) NOT NULL,
+    `descripcion` VARCHAR(1000), 
+    `fechaInicio` TIMESTAMP NOT NULL , 
+    `fechaFin` TIMESTAMP NOT NULL ,    
+    PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_spanish_ci;
