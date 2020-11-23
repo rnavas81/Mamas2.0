1f41a4b57269968d755a4c74a18a0c4cbe80784a
dario <darioleong1997@gmail.com>
2020-11-23 14:29:32 +0100

    Actualizadas tablas

--- docs/Alumnos_examenes.sql
+++ docs/Alumnos_examenes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `Mamas`.`Alumnos_examenes` ( 
     `idAlumno` INT NOT NULL , 
-    `idExamen` INT NOT NULL ,
-    `nota` INT NOT NULL DEFAULT '1', 
-    `realizado` INT NOT NULL DEFAULT '1',    
+    `idExamen` INT NOT NULL , 
+    `nota` INT NULL DEFAULT NULL , 
+    `realizado` INT NOT NULL DEFAULT '0' , 
     PRIMARY KEY (`idAlumno`, `idExamen`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_spanish_ci;
--- docs/Examenes.sql
+++ docs/Examenes.sql
@@ -5,6 +5,6 @@ CREATE TABLE `Mamas`.`Examenes` (
     `activo` TINYINT NOT NULL DEFAULT '0' ,
     `nombre` VARCHAR(500) NOT NULL,
     `descripcion` VARCHAR(1000), 
-    `fechaInicio` TIMESTAMP NOT NULL , 
-    `fechaFin` TIMESTAMP NOT NULL ,    
+    `fechaInicio` TIMESTAMP , 
+    `fechaFin` TIMESTAMP ,    
     PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_spanish_ci;
